ee1c826ee1c4c0b202a3204dbb5d4958567bfc5c
DariyaSazonova <Sazonovade@gmail.com>
2023-01-03 12:09:38 +0300

    Задача в группах: по нахождению вохможных координат точек в четверти

---
+++ Seminar_1/Базовые_алгоритмы_задачи_в_группах/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите программу, которая по заданному номеру четверти показывает 
+// диапазон возможных координат точек в этой четверти (x и y)
+
+Console.WriteLine("введите номер четверти, от 1 до 4");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number < 1 || number > 4)
+{
+    Console.Write("Это не номер чеверти!");
+    return;
+}
+
+if ( number == 1 )
+{
+    Console.Write("X > 0; Y > 0");
+}
+if ( number == 2 )
+{
+    Console.Write("X < 0; Y > 0");
+}
+if ( number == 3 )
+{
+    Console.Write("X < 0; Y < 0");
+}
+if ( number == 4 )
+{
+    Console.Write("X > 0; Y < 0");
+}
